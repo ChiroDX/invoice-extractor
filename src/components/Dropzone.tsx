97896fe72f45ad9ad1b1b464acd2439365381e4c
RakkiDI <tuanakki@gmail.com>
2022-01-09 23:23:15 +0100

    Dropzone with accepts

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -1,39 +1,98 @@
 
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { useDropzone } from 'react-dropzone'
 import { Container, Typography } from '@mui/material';
 import SaveAltIcon from '@mui/icons-material/SaveAlt';
 
+const baseStyle = {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '20px',
+    borderWidth: 2,
+    borderRadius: 2,
+    borderColor: '#eeeeee',
+    borderStyle: 'dashed',
+    backgroundColor: '#fafafa',
+    color: '#bdbdbd',
+    outline: 'none',
+    transition: 'border .24s ease-in-out'
+};
 
-const Dropzone = () => {
+const activeStyle = {
+    borderColor: '#2196f3'
+};
+
+const acceptStyle = {
+    borderColor: '#00e676'
+};
+
+const rejectStyle = {
+    borderColor: '#ff1744'
+};
+
+
+const FileDropzone = () => {
     const onDrop = useCallback(acceptedFiles => {
         // Do something with the files
     }, [])
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+    const { acceptedFiles, fileRejections,
+        getRootProps, getInputProps,
+        isDragActive, isDragAccept, isDragReject } = useDropzone({
+            onDrop,
+            accept: "application/pdf"
+        })
+
+    const style = useMemo(() => ({
+        ...baseStyle,
+        ...(isDragActive ? activeStyle : {}),
+        ...(isDragAccept ? acceptStyle : {}),
+        ...(isDragReject ? rejectStyle : {})
+    }), [
+        isDragActive,
+        isDragReject,
+        isDragAccept
+    ]);
+
+    const acceptedFileItems = acceptedFiles.map(file => (
+        // @ts-ignore
+        <li key={file.path}>
+            {/* @ts-ignore*/}
+            {file.path} - {file.size} bytes
+        </li>
+    ));
+
+    const fileRejectionItems = fileRejections.map(({ file, errors }) => (
+        // @ts-ignore
+        <li key={file.path}>
+            {/* @ts-ignore*/}
+            {file.path} - {file.size} bytes
+            <ul>
+                {errors.map(e => (
+                    <li key={e.code}>{e.message}</li>
+                ))}
+            </ul>
+        </li>
+    ));
 
     return (
         <Container maxWidth="sm">
-            <div {...getRootProps()} style={{ display: "grid", alignItems: "center", backgroundColor: '#cfe8fc', height: '300px' }}>
-                <input {...getInputProps({
-                    //Its important to filter, because somehow .cdrt files makes the HTML5 Filepicker to crash
-                    //Add more filters if any forgotten
-                    accept: "image/*, .eps"
-                })} />
-                {
-                    isDragActive ?
-                        <div ><Typography variant="h1">Datei hier reinziehen ...</Typography></div>
-                        :
-                        <div>
-                            <Typography variant="h5">
-                                <div style={{ textAlign: "center" }}> Datei hier reinziehen,<br /> oder für die Dateiauswahl klicken</div>
-                                <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}><SaveAltIcon style={{ fontSize: "2.5em" }} /></div>
-                            </Typography>
-                        </div>
-
-                }
+            <div className="container">
+                {/* @ts-ignore */}
+                <div {...getRootProps({ style })}>
+                    <input {...getInputProps()} />
+                    <p>Drag 'n' drop pdf file in here</p>
+                </div>
             </div>
+            <aside>
+                <h4>Accepted files</h4>
+                <ul>{acceptedFileItems}</ul>
+                <h4>Rejected files</h4>
+                <ul>{fileRejectionItems}</ul>
+            </aside>
         </Container>
     )
 }
 
-export default Dropzone;
+export default FileDropzone;
